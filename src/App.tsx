d155f19bff93de7320d6fe7ee7577687552b2564
Kyle Lemire <hello@klemire.com>
2021-09-11 19:27:28 -0500

    save

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,6 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>Hello Vite + React!</p>
         <Counter />
       </header>
       <main>
